44b0463a133a1d8e27a4c3be089b1da7abb23c66
dom b <bellybuchelli1234@gmail.com>
2022-02-04 09:33:00 -0500

    first funtion, array, output

---
+++ script.js
@@ -0,0 +1,33 @@
+// list of features to build 
+// 1) have js disply the world of brickcoinect
+// 2) have the pacman move up and down 
+
+var world = [
+    [2,2,2,2,2,2,2,2,2,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2]
+];
+
+function displayWorld(){
+    var output= "";
+
+    for(var i=0; i<world.length; i++){
+        output += "\n<div class='row'>";
+        for(var j=0; j<world[i].length; j++){
+            if(world[i][j] == 2)
+            output += "\n\t<div class='brick'></div";
+            else if(world[i][j] == 1)
+            output += "\n\t<div class='coin'></div";
+            if(world[i][j] == 0)
+            output += "\n\t<div class='empty'></div";
+        }
+        output+= "\n</div>"
+    }
+    console.log(output)
+}
